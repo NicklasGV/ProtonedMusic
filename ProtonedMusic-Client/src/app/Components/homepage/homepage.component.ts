1efc7e791172026856be6477796647e69a1a5237
NicklasGV <nvedeby@gmail.com>
2023-09-18 12:29:31 +0200

    Added components

---
+++ ProtonedMusic-Client/src/app/Components/homepage/homepage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
