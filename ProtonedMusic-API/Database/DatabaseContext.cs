b6ccbbbf0075314d6c6c0d0ca08f23d23feda1b7
NiklasWeinreich <Niklasweinreichhansen@hotmail.com>
2023-09-18 12:54:48 +0200

    hey

--- ProtonedMusic-API/Database/DatabaseContext.cs
+++ ProtonedMusic-API/Database/DatabaseContext.cs
@@ -1,6 +1,28 @@
-﻿namespace ProtonedMusic_API.Database
+﻿
+using ProtonedMusic_API.Database.Entities;
+
+namespace ProtonedMusic_API.Database
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>().HasData(
+                new Products
+                {
+                    id = 1,
+                    ProductName = "ProductNameTest",
+                    ProductCategory = "ProductCategoryTest",
+                    ProductDescription = "ProductDescriptionTest",
+                    ProductPrice = 200,
+                    ProductDate = 18 - 09 - 2023
+                });
+
+                
+        }
     }
 }
--- ProtonedMusic-API/GlobalUsings.cs
+++ ProtonedMusic-API/GlobalUsings.cs
@@ -1,6 +1,3 @@
-﻿namespace ProtonedMusic_API
-{
-    public class GlobalUsings
-    {
-    }
-}
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
