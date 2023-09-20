87eaa5129f9e245d2e9ccf36a2d16bdec6ce157b
NiklasWeinreich <Niklasweinreichhansen@hotmail.com>
2023-09-20 14:53:56 +0200

    ProductModel Implemented

---
+++ ProtonedMusic-Client/src/app/Models/ProductModel.ts
@@ -0,0 +1,7 @@
+export class ProductModel {
+    id: Number = 0;
+    productName: String = "";
+    productCategory: String = "";
+    productPrice: Number = 0;
+    productDescription: String = "";
+}
