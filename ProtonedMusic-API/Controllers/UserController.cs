1d691b28ac5391bb91823f7816325319685d3134
Joey Oliver Høffding Knutsson <joey0177@elev.tec.dk>
2023-09-21 10:43:14 +0200

    Test

---
+++ ProtonedMusic-API/Controllers/UserController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Environ.API.Authorization;
+using Environ.API.DTO.LoginDTO;
+
+namespace Environ.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtUtils _jwtUtils;
+
+        public UserController(IUserService userService, IJwtUtils jwtUtils, IUserRepository userRepository)
+        {
+            _userService = userService;
+            _jwtUtils = jwtUtils;
+            _userRepository = userRepository;
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest login)
+        {
+            try
+            {
+                LoginResponse user = await _userService.AuthenticateUser(login);
+                if (user == null)
+                {
+                    return Unauthorized();
+                }
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("{userId}")]
+        public async Task<IActionResult> FindById([FromRoute] int userId)
+        {
+            try
+            {
+                UserResponse userResponse = await _userService.FindById(userId);
+
+                if (userResponse == null)
+                {
+                    return NotFound();
+                }
+                return Ok(userResponse);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("{userId}")]
+        public async Task<IActionResult> UpdateById([FromRoute] int userId, [FromBody] UserRequest updateUser)
+        {
+            try
+            {
+                var userResponse = await _userService.UpdateById(userId, updateUser);
+
+                if (userResponse == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(userResponse);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("{userId}")]
+        public async Task<IActionResult> DeleteById([FromRoute] int userId)
+        {
+            try
+            {
+                var userResponse = await _userService.DeleteById(userId);
+                if (userResponse == null)
+                {
+                    return NotFound();
+                }
+                return Ok(userResponse);
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                List<UserResponse> users = await _userService.GetAll();
+
+                if (users.Count == 0)
+                {
+                    return NoContent();
+                }
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] UserRequest newUser)
+        {
+            try
+            {
+                UserResponse userResponse = await _userService.CreateUser(newUser);
+                return Ok(userResponse);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
---
+++ ProtonedMusic.Service/Services/UserService.cs
@@ -0,0 +1,137 @@
+﻿using Environ.API.Authorization;
+using Environ.API.DTO.LoginDTO;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Environ.API.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtUtils _jwtUtils;
+
+
+        public UserService(IUserRepository userRepository, IJwtUtils jwtUtils)
+        {
+            _userRepository = userRepository;
+            _jwtUtils = jwtUtils;
+        }
+
+        public static UserResponse MapUserToUserResponse(User user)
+        {
+            UserResponse response = new UserResponse
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Role = user.Role
+            };
+            if (user.Address != null)
+            {
+                response.Address = new UserAddressResponse
+                {
+                    Id = user.Address.Id,
+                    AddressLineOne = user.Address.AddressLineOne,
+                    AddressLineTwo = user.Address.AddressLineTwo,
+                    Country = user.Address.Country,
+                    City = user.Address.City,
+                    Postal = user.Address.Postal,
+                    State = user.Address.State,
+                };
+            }
+            return response;
+        }
+
+        private static User MapUserRequestToUser(UserRequest userRequest)
+        {
+            User user = new User
+            {
+                FirstName = userRequest.FirstName,
+                LastName = userRequest.LastName,
+                Email = userRequest.Email,
+                Password = userRequest.Password ?? string.Empty,
+                Role = userRequest.Role
+            };
+            return user;
+        }
+
+        public async Task<List<UserResponse>> GetAll()
+        {
+            List<User> users = await _userRepository.GetAll();
+
+            if (users == null)
+            {
+                throw new ArgumentException();
+            }
+            return users.Select(user => MapUserToUserResponse(user)).ToList();
+        }
+
+        public async Task<UserResponse> FindById(int userId)
+        {
+            var user = await _userRepository.FindById(userId);
+
+            if (user != null)
+            {
+                return MapUserToUserResponse(user);
+            }
+
+            return null;
+        }
+
+        public async Task<UserResponse> CreateUser(UserRequest newUser)
+        {
+            var user = await _userRepository.CreateUser(MapUserRequestToUser(newUser));
+            if (user == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return MapUserToUserResponse(user);
+        }
+
+        public async Task<UserResponse> DeleteById(int userId)
+        {
+            var user = await _userRepository.DeleteById(userId);
+
+            if(user != null)
+            {
+                return MapUserToUserResponse(user);
+            }
+            return null;
+        }
+
+        public async Task<UserResponse> UpdateById(int userId, UserRequest updateUser)
+        {
+            var user = MapUserRequestToUser(updateUser);
+            var insertedUser = await _userRepository.UpdateById(userId, user);
+
+            if (insertedUser != null)
+            {
+                return MapUserToUserResponse(insertedUser);
+            }
+
+            return null;
+        }
+
+        public async Task<LoginResponse> AuthenticateUser(LoginRequest login)
+        {
+            User user = await _userRepository.FindByEmail(login.Email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (user.Password == login.Password)
+            {
+                LoginResponse response = new()
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Role = user.Role,
+                    Token = _jwtUtils.GenerateJwtToken(user)
+                };
+                return response;
+            }
+            return null;
+        }
+    }
+}
