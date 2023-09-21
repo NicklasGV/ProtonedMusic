c6c7468815dbd99ec9123ad21e4c02c24f044c00
NiklasWeinreich <Niklasweinreichhansen@hotmail.com>
2023-09-21 07:51:28 +0200

    test

--- ProtonedMusic-Client/src/app/app.module.ts
+++ ProtonedMusic-Client/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './Shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
