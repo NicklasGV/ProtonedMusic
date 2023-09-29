576d6564861e7f408b8bdb4af82a13efa47e12de
Nicklas Vedeby <nick020a@elev.tec.dk>
2023-09-29 08:07:37 +0200

    JsonIgnore Added

--- ProtonedMusic.Utility/Models/ProductModel.cs
+++ ProtonedMusic.Utility/Models/ProductModel.cs
@@ -3,6 +3,7 @@
     public class ProductModel
     {
         [Key]
+        [JsonIgnore]
         public int Id { get; set; }
         [Required]
         public string ProductName { get; set; } = string.Empty;
