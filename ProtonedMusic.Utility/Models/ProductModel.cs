ac15c6d2dd98ec77860ead49ac200654e7b6ce01
NiklasWeinreich <Niklasweinreichhansen@hotmail.com>
2023-09-29 10:06:55 +0200

    DKK

--- ProtonedMusic.Utility/Models/ProductModel.cs
+++ ProtonedMusic.Utility/Models/ProductModel.cs
@@ -7,7 +7,6 @@ namespace ProtonedMusic.Utility.Models
     public class ProductModel
     {
         [Key]
-        [JsonIgnore]
         public int Id { get; set; }
         [Required]
         public string ProductName { get; set; } = string.Empty;
