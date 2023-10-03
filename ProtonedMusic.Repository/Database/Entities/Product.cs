19e00a5a10d4048b7a6a472dfbc96f0d44e408cc
Nicklas Vedeby <niv@praktikcenter.lan>
2023-10-03 14:53:33 +0200

    Category and Product DTO added

---
+++ ProtonedMusic.Repository/Database/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtonedMusic.Repository.Database.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(80)")]
+        public string Name { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; } = 0;
+        [Column(TypeName = "nvarchar(500)")]
+        public string Description { get; set; } = string.Empty;
+        public List<ProductCategory> ProductCategories { get; set; } = new();
+    }
+}
