3eab7101e1ea3348ff2eeb47a198dbd599bf114a
NiklasWeinreich <Niklasweinreichhansen@hotmail.com>
2023-10-11 13:08:31 +0200

    as

--- ProtonedMusic-Client/src/app/Shared/navbar/navbar.component.ts
+++ ProtonedMusic-Client/src/app/Shared/navbar/navbar.component.ts
@@ -3,39 +3,34 @@ import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { User, resetUser } from 'src/app/Models/UserModel';
 import { AuthService } from 'src/app/Services/auth.service';
-import { UserService } from 'src/app/Services/user.service';
-
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   currentUser: User = resetUser();
   roleChecker: string = 'Admin';
   tt: boolean = false;
 
-  constructor(private router: Router, private authService:AuthService, private userService: UserService) {
+  constructor(
+    private authService: AuthService,
 
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe(x => this.currentUser = x);
-
-    console.log('Bruger logger ind:', this.authService.currentUserValue); 
+    this.authService.currentUser.subscribe((x) => (this.currentUser = x));
+    console.log(this.currentUser.role);
+    console.log('Bruger logger ind:', this.authService.currentUserValue);
   }
 
-  roleCheck() : boolean
-  {
-
-    if (this.currentUser.role == this.roleChecker)
-    {
+  roleCheck(): boolean {
+    if (this.currentUser.role == this.roleChecker) {
       return true;
     }
     return false;
   }
-
 }
