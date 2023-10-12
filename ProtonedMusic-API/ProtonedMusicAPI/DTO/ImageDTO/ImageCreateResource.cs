2a7d04aec0412a0c3d8f161f1b7897100fadd542
Niklas Wienriech <niw@praktikcenter.lan>
2023-10-12 08:36:02 +0200

    image

--- ProtonedMusic-API/ProtonedMusicAPI/DTO/ImageDTO/ImageCreateResource.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/DTO/ImageDTO/ImageCreateResource.cs
@@ -2,5 +2,10 @@
 {
     public class ImageCreateResource
     {
+        public Guid ImageId { get; set; }
+        public string FileExtension { get; set; }
+        public IFormFile ImageFile { get; set; }
+
+        public int ProductId {  get; set; }
     }
 }
