20afea1356cfb934d5ad62d26ddce32a514ab50a
Nicklas Vedeby <niv@praktikcenter.lan>
2023-11-06 14:08:06 +0100

    Small changes to icon

--- ProtonedMusic-Client/src/app/Shared/footer/footer.component.ts
+++ ProtonedMusic-Client/src/app/Shared/footer/footer.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
+import { User, resetUser } from 'src/app/Models/UserModel';
+import { EmailService } from 'src/app/Services/email.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  currentUser: User = resetUser();
+  roleChecker: string = 'Admin';
 
-  constructor() { }
+  constructor(private authService: AuthService, private router:Router, private emailService: EmailService) {
+    this.authService.currentUser.subscribe((x) => (this.currentUser = x));
+  }
 
   ngOnInit(): void {
+  }
+
+  roleCheck(): boolean {
+    if (this.currentUser.role == this.roleChecker) {
+      return true;
+    }
+    return false;
   }
 
 }
