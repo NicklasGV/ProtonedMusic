415cbff2d5ee3572e8a57915fc19c31290bc9528
Nicklas Vedeby <niv@praktikcenter.lan>
2023-11-10 13:25:02 +0100

    Changed cors location

--- ProtonedMusic-API/ProtonedMusicAPI/Program.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/Program.cs
@@ -40,25 +40,26 @@ namespace ProtonedMusicAPI
 
             builder.Services.AddScoped<IEmailService, EmailService>();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                    .WithOrigins("https://protonedmusic.com")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                );
+            });
+
             //builder.Services.AddCors(options =>
             //{
             //    options.AddPolicy("CorsPolicy", builder => builder
-            //        .WithOrigins("https://protonedmusic.com")
+            //        .WithOrigins("http://localhost:4200")
             //        .AllowAnyHeader()
             //        .AllowAnyMethod()
             //        .AllowCredentials()
             //    );
             //});
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", builder => builder
-                    .WithOrigins("http://localhost:4200")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials()
-                );
-            });
             builder.Services.AddDbContext<DatabaseContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("ConString"));
