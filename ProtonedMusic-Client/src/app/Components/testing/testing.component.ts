e75d7c615fe2ca8667a6620bd8d7e199a2a91d9b
Nicklas Vedeby <niv@praktikcenter.lan>
2023-11-20 14:55:35 +0100

    HEHE

--- ProtonedMusic-Client/src/app/Components/testing/testing.component.ts
+++ ProtonedMusic-Client/src/app/Components/testing/testing.component.ts
@@ -9,10 +9,28 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./testing.component.css']
 })
 export class TestingComponent implements OnInit {
+  audio = new Audio();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.playSound();
+  }
+
+  playSound() {
+    this.audio.src = "../../assets/music/Lady.mp3";
+    this.audio.load();
+    this.audio.play();
+  }
+
+  pauseSound() {
+    this.audio.src = "../../assets/music/Lady.mp3";
+    this.audio.pause
+  }
+
+  startSound() {
+    this.audio.src = "../../assets/music/Lady.mp3";
+    this.audio.play();
   }
 
 }
