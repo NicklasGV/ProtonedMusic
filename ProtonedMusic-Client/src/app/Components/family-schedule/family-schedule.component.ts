cafbf1b31ea4a64958e449145fb74b73eb000681
Nicklas Vedeby <niv@praktikcenter.lan>
2023-12-12 08:31:32 +0100

    Small calendar added not working yet

--- ProtonedMusic-Client/src/app/Components/family-schedule/family-schedule.component.ts
+++ ProtonedMusic-Client/src/app/Components/family-schedule/family-schedule.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-family-schedule',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatNativeDateModule, MatDatepickerModule],
   templateUrl: './family-schedule.component.html',
   styleUrl: './family-schedule.component.css'
 })
 export class FamilyScheduleComponent {
+  selected?: Date;
 
 }
