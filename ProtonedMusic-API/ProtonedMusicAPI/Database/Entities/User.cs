ca1599c7170ca88adf769ad3c5f2ec9e45a77fe9
Nicklas Vedeby <niv@praktikcenter.lan>
2023-12-21 08:22:35 +0100

    Small changes to user and profile

--- ProtonedMusic-API/ProtonedMusicAPI/Database/Entities/User.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/Database/Entities/User.cs
@@ -6,34 +6,34 @@
         public int Id { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
         [Column(TypeName = "nvarchar(50)")]
-        public string LastName { get; set; }
-
+        public string LastName { get; set; } = string.Empty;
+        [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string Email { get; set; }
-
+        [Required]
         [Column(TypeName = "nvarchar(500)")]
         public string Password { get; set; }
 
         public Role Role { get; set; }
-        public AddonRoles AddonRoles { get; set; }
+        public AddonRoles AddonRoles { get; set; } = AddonRoles.None;
 
         [Column(TypeName = "int")]
-        public int PhoneNumber { get; set; }
+        public int PhoneNumber { get; set; } = int.MinValue;
 
         [Column(TypeName = "nvarchar(50)")]
-        public string Address { get; set; }
+        public string Address { get; set; } = string.Empty;
 
         [Column(TypeName = "nvarchar(50)")]
-        public string City { get; set; }
+        public string City { get; set; } = string.Empty;
 
         [Column(TypeName = "int")]
-        public int Postal { get; set; }
+        public int Postal { get; set; } = int.MinValue;
 
         [Column(TypeName = "nvarchar(50)")]
-        public string Country { get; set; }
+        public string Country { get; set; } = string.Empty;
 
         public string? ProfilePicturePath { get; set; }
 
