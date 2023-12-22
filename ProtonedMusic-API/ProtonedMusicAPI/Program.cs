fe5b4baf469fefe2529a4529e987db295c6e176d
Joey Oliver Høffding Knutsson <joey0177@elev.tec.dk>
2023-12-22 08:35:13 +0100

    Jul

--- ProtonedMusic-API/ProtonedMusicAPI/Program.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/Program.cs
@@ -28,6 +28,9 @@ namespace ProtonedMusicAPI
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IJwtUtils, JwtUtils>();
 
+            builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+            builder.Services.AddScoped<IArtistService, ArtistService>();
+
             builder.Services.AddScoped<IMusicRepository, MusicRepository>();
             builder.Services.AddScoped<IMusicService, MusicService>();
 
