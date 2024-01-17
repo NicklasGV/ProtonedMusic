58d5155f9d5e35e91c437863ba43f9a4017872db
Niklas Wienriech <niw@praktikcenter.lan>
2024-01-17 13:31:09 +0100

    Update OrderHistoryRequest and IOrderHistoryRepository

--- ProtonedMusic-API/ProtonedMusicAPI/Interfaces/IOrderHistoryRepository.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/Interfaces/IOrderHistoryRepository.cs
@@ -5,6 +5,6 @@
         Task<List<Order>> GetOrdersByCustomerId(string customerId); // Ændring her
         Task<Order> GetOrdersById(int orderId);
         Task<Order> GetOrdersByPaymentId(string paymentId); // Tilføj denne metode
-        Task<Order> CreateOrder(int customerId, List<ItemProduct> items, string orderNumber);
+        Task<Order> CreateOrder(Order order);
     }
 }
