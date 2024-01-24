c5924e1adfc81cdf908c9d8d52706e5a60453f8c
Niklas Wienriech <niw@praktikcenter.lan>
2024-01-24 14:42:51 +0100

    comment

--- ProtonedMusic-Client/src/app/Components/merchandise-product/merchandise-product.component.ts
+++ ProtonedMusic-Client/src/app/Components/merchandise-product/merchandise-product.component.ts
@@ -1,46 +1,49 @@
-import {  Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductModel, resetProducts } from 'src/app/Models/ProductModel';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ProductService } from 'src/app/Services/product.service';
-import { CartItem, } from 'src/app/Models/CartModel';
+import { CartItem } from 'src/app/Models/CartModel';
 import { CartService } from 'src/app/Services/cart.service';
-
 
 @Component({
   selector: 'app-merchandise-product',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './merchandise-product.component.html',
-  styleUrls: ['./merchandise-product.component.css']
+  styleUrls: ['./merchandise-product.component.css'],
 })
 export class MerchandiseProductComponent implements OnInit {
   products: ProductModel = resetProducts();
+  //Nu jeg her
   product: ProductModel[] = [];
   itemlength = 0;
   itemsQuantity = 0;
+  //nu jeg der
   productList: ProductModel[] = [];
   currentIndex: number = 0;
   itemsPerPage: number = 4;
-
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
     private cartService: CartService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {this.productService.getProductById(params['id']).subscribe({
-      next: (product) => {
-        this.products = product;
-            product.beforePrice = product.price;
-            if (product.discountProcent > 0) {
-              product.price = product.price - (product.price / 100 * product.discountProcent);
-            }
-        }
-    });});
-    this.productService.getAllProducts().subscribe(products => {
+    this.route.params.subscribe((params) => {
+      this.productService.getProductById(params['id']).subscribe({
+        next: (product) => {
+          this.products = product;
+          product.beforePrice = product.price;
+          if (product.discountProcent > 0) {
+            product.price =
+              product.price - (product.price / 100) * product.discountProcent;
+          }
+        },
+      });
+    });
+    this.productService.getAllProducts().subscribe((products) => {
       this.productList = products;
       this.loadProducts();
     });
@@ -54,7 +57,7 @@ export class MerchandiseProductComponent implements OnInit {
       price: products.price,
       quantity: 1,
       name: products.name,
-      picturePath: products.productPicturePath
+      picturePath: products.productPicturePath,
     } as CartItem;
     this.cartService.addToCart(item);
   }
@@ -71,7 +74,7 @@ export class MerchandiseProductComponent implements OnInit {
       const remainingItems = endIndex - totalProducts;
       this.product = [
         ...this.productList.slice(startIndex, totalProducts),
-        ...this.productList.slice(0, remainingItems)
+        ...this.productList.slice(0, remainingItems),
       ];
     }
   }
@@ -81,7 +84,9 @@ export class MerchandiseProductComponent implements OnInit {
   }
 
   previousProducts(): void {
-    this.currentIndex = (this.currentIndex - 1 + this.productList.length) % this.productList.length;
+    this.currentIndex =
+      (this.currentIndex - 1 + this.productList.length) %
+      this.productList.length;
     this.loadProducts();
   }
 }
