b3603d1a222d07b9e684749174d7f7c3b4c4b299
Niklas Wienriech <niw@praktikcenter.lan>
2024-01-25 14:45:41 +0100

    fix

--- ProtonedMusic-Client/src/app/Components/ordersuccess/ordersuccess.component.ts
+++ ProtonedMusic-Client/src/app/Components/ordersuccess/ordersuccess.component.ts
@@ -2,7 +2,6 @@ import { CartService } from 'src/app/Services/cart.service';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartItem } from 'src/app/Models/CartModel';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ordersuccess',
@@ -19,13 +18,13 @@ export class OrdersuccessComponent {
   ngOnInit(): void {
     // Hent data fra localStorage, inklusive købte varer
     const temporaryCartItems = this.cartService.getFullCart();
-    console.log('CartItems from localStorage:', temporaryCartItems);
+    console.log(temporaryCartItems);
 
     // Opdater currentCartSubject med de midlertidige varer
     this.cartService.currentCartSubject.next(temporaryCartItems);
 
     // Tøm indkøbskurven efter at have hentet data
-    this.cartService.clearCart();
+     this.cartService.clearCart();
 
     // Nu har du adgang til købte varer i temporaryCartItems
     this.cartItems = temporaryCartItems;
