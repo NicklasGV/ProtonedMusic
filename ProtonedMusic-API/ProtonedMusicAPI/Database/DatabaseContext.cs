f50b0a40508be38b83a0afc2f5e91d97c4db4eb7
Niklas Wienriech <niw@praktikcenter.lan>
2024-02-16 10:32:20 +0100

    Removed unnecessary code

--- ProtonedMusic-API/ProtonedMusicAPI/Database/DatabaseContext.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/Database/DatabaseContext.cs
@@ -74,15 +74,6 @@ namespace ProtonedMusicAPI.Database
 
             modelBuilder.Entity<ProductCategory>().HasKey(pc => new { pc.ProductId, pc.CategoryId });
 
-            modelBuilder.Entity<CalendarContent>().HasData(new CalendarContent
-            {
-                Id =1,
-                Title = "Title",
-                Content = "This is a test content of a calendar content",
-                Date = new DateTime(),
-                ArtistId = 1,
-            });
-
             modelBuilder.Entity<FooterPost>().HasData(new FooterPost
             {
                 Id = 1,
@@ -93,114 +84,13 @@ namespace ProtonedMusicAPI.Database
                 Phonenumber = "+45 12345678"
             });
 
-            modelBuilder.Entity<Product>().HasData(new Product
-            {
-                Id = 1,
-                Name = "Rock Shirt",
-                Price = 399.95M,
-                Description = "Testproduct for seeing 2 categories",
-                IsDiscounted = true,
-                DiscountProcent = 20,
-            },
-            new Product
-            {
-                Id = 2,
-                Name = "Normal Cap",
-                Price = 560,
-                Description = "Testproduct cap",
-            },
-            new Product
-            {
-                Id = 3,
-                Name = "Raid Shadow Legends",
-                Price = 299.95M,
-                Description = "Today's video is sponsored by Raid Shadow Legends, one of the biggest mobile role-playing games of 2019 and it's totally free! Currently almost 10 million users have joined Raid over the last six months, and it's one of the most impressive games in its class with detailed models, environments and smooth 60 frames per second animations! All the champions in the game can be customized with unique gear that changes your strategic buffs and abilities! So what are you waiting for? Go to the video description! Good luck and I'll see you there!"
-            },
-            new Product
-            {
-                Id = 4,
-                Name = "Pop song",
-                Price = 760,
-                Description = "Testproduct song",
-                IsDiscounted = true,
-                DiscountProcent = 80,
-            });
-
-            modelBuilder.Entity<Category>().HasData(new Category
-            {
-                Id = 1,
-                Name = "T-Shirt"
-            },
-            new Category
-            {
-                Id = 2,
-                Name = "Pop"
-            },
-            new Category
-            {
-                Id = 3,
-                Name = "Cap"
-            },
-            new Category
-            {
-                Id = 4,
-                Name = "Special"
-            });
-
-            modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
-            {
-                CategoryId = 1,
-                ProductId = 1,
-            }, 
-            new ProductCategory
-            {
-                CategoryId = 1,
-                ProductId = 2
-            },
-            new ProductCategory
-            {
-                CategoryId = 3,
-                ProductId = 2,
-            },
-            new ProductCategory
-            {
-                CategoryId = 4,
-                ProductId = 3,
-            },
-            new ProductCategory
-            {
-                CategoryId = 2,
-                ProductId = 4,
-            });
-
-            modelBuilder.Entity<Event>().HasData(
-                new Event
-                {
-                    Id = 1,
-                    Title = "First Event",
-                    Description = "Test event",
-                    Price = 249.95M,
-                    Created = DateTime.Now,
-                    TimeofEvent = new DateTime(2023,5,2,23,23,00),
-                },
-                new Event
-                {
-                    Id = 2,
-                    Title = "Super Event",
-                    Description = "Test 2 for testing making events",
-                    Price = 546.95M,
-                    Created = DateTime.Now,
-                    TimeofEvent = new DateTime(2023, 9, 17, 13, 20, 00),
-                }); ;
-
-
             modelBuilder.Entity<User>().HasData(
                 new User
                 {
                     Id = 1,
-                    FirstName = "Joey",
-                    LastName = "Test",
-                    Email = "testmail1",
+                    FirstName = "Protoned",
+                    LastName = "Music",
+                    Email = "noreply@protonedmusic.com",
                     Password = BCrypt.Net.BCrypt.HashPassword("Passw0rd"),
                     Role = Role.Admin,
                     PhoneNumber = 12345678,
@@ -208,53 +98,8 @@ namespace ProtonedMusicAPI.Database
                     City = "Test By",
                     Postal = 1234,
                     Country = "Denmark"
-                },
-                new User
-                {
-                    Id = 2,
-                    FirstName = "Børge",
-                    LastName = "Jep",
-                    Email = "testmail2",
-                    Password = BCrypt.Net.BCrypt.HashPassword("Password"),
-                    Role = Role.Customer,
-                    PhoneNumber = 12345679,
-                    Address = "Test Vej 2",
-                    City = "Test By",
-                    Postal = 1234,
-                    Country = "Denmark"
                 }
                 );
-
-            modelBuilder.Entity<News>().HasData(new News
-            {
-                Id = 1,
-                Title = "DATABASE GOT RESET",
-                Text = "Sorry if you lost important data or something funny, but hey whoever needed to resetting the database needed it. You can see under here when it last got reset",
-                DateTime = DateTime.Now,
-            },
-            new News
-            {
-                Id = 2,
-                Title = "Website Running!",
-                Text = "So ProtonedMusic's website is now up and running!",
-                DateTime = new DateTime(2022, 01, 01),
-            },
-            new News
-            {
-                Id = 3,
-                Title = "NEW SONG OUT",
-                Text = "Check out my new song in merchandise",
-                DateTime = new DateTime(2023, 08, 12),
-            });
-
-            modelBuilder.Entity<NewsLike>().HasData(new NewsLike
-            {
-                Id = 1,
-                user_Id = 1,
-                news_Id = 1,
-                DateTime = DateTime.Now,
-
-            });
 
             modelBuilder.Entity<FrontpagePost>().HasData(new FrontpagePost
             {
@@ -278,71 +123,189 @@ namespace ProtonedMusicAPI.Database
                 FrontpagePicturePath = "assets/img/bannerLogo3.jpg"
             });
 
-            modelBuilder.Entity<Music>().HasData(new Music
-            {
-                Id = 1,
-                SongName = "Chipi chipi chapa chapa",
-                Album = "Chipi chipi",
-                SongFilePath = "assets/music/chibichibi.mp3",
-                SongPicturePath = "assets/img/chipichapa.gif"
-            },
-            new Music
-            {
-                Id = 2,
-                SongName = "FlipFlop",
-                Album = "Bjørn",
-                SongFilePath = "assets/music/audio2.mp3",
-                SongPicturePath = ""
-            },
-            new Music
-            {
-                Id = 3,
-                SongName = "Chatter",
-                Album = "Around the worlds",
-                SongFilePath = "assets/music/audio1.mp3",
-                SongPicturePath = ""
-            });
-
             modelBuilder.Entity<Artist>().HasData(new Artist
             {
                 Id = 1,
                 UserId = 1,
-                Name = "Joey Testoe",
-                Info = "Bedste Sanger",
+                Name = "ProtonedMusic",
+                Info = "DJ",
             });
 
-            modelBuilder.Entity<ArtistSong>().HasData(new ArtistSong
-            {
-                Id = 1,
-                ArtistId = 1,
-                MusicId = 1,
-            },
-            new ArtistSong
-            {
-                Id = 2,
-                ArtistId = 1,
-                MusicId = 2,
-            },
-            new ArtistSong
-            {
-                Id = 3,
-                ArtistId = 1,
-                MusicId = 3,
-            });
+            //modelBuilder.Entity<CalendarContent>().HasData(new CalendarContent
+            //{
+            //    Id =1,
+            //    Title = "Title",
+            //    Content = "This is a test content of a calendar content",
+            //    Date = new DateTime(),
+            //    ArtistId = 1,
+            //});
 
-            modelBuilder.Entity<ArtistLink>().HasData(new ArtistLink
-            {
-                Id = 1,
-                ArtistId = 1,
-                LinkId = 1,
-            });
+            //modelBuilder.Entity<Product>().HasData(new Product
+            //{
+            //    Id = 1,
+            //    Name = "Rock Shirt",
+            //    Price = 399.95M,
+            //    Description = "Testproduct for seeing 2 categories",
+            //    IsDiscounted = true,
+            //    DiscountProcent = 20,
+            //},
+            //new Product
+            //{
+            //    Id = 2,
+            //    Name = "Normal Cap",
+            //    Price = 560,
+            //    Description = "Testproduct cap",
+            //},
+            //new Product
+            //{
+            //    Id = 3,
+            //    Name = "Raid Shadow Legends",
+            //    Price = 299.95M,
+            //    Description = "Today's video is sponsored by Raid Shadow Legends, one of the biggest mobile role-playing games of 2019 and it's totally free! Currently almost 10 million users have joined Raid over the last six months, and it's one of the most impressive games in its class with detailed models, environments and smooth 60 frames per second animations! All the champions in the game can be customized with unique gear that changes your strategic buffs and abilities! So what are you waiting for? Go to the video description! Good luck and I'll see you there!"
+            //},
+            //new Product
+            //{
+            //    Id = 4,
+            //    Name = "Pop song",
+            //    Price = 760,
+            //    Description = "Testproduct song",
+            //    IsDiscounted = true,
+            //    DiscountProcent = 80,
+            //});
 
-            modelBuilder.Entity<Link>().HasData(new Link
-            {
-                Id = 1,
-                Title = "Discord",
-                LinkAddress = "https://discord.gg/Jt4rwUZGGS"
-            });
+            //modelBuilder.Entity<Category>().HasData(new Category
+            //{
+            //    Id = 1,
+            //    Name = "T-Shirt"
+            //},
+            //new Category
+            //{
+            //    Id = 2,
+            //    Name = "Pop"
+            //},
+            //new Category
+            //{
+            //    Id = 3,
+            //    Name = "Cap"
+            //},
+            //new Category
+            //{
+            //    Id = 4,
+            //    Name = "Special"
+            //});
+
+            //modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+            //{
+            //    CategoryId = 1,
+            //    ProductId = 1,
+            //}, 
+            //new ProductCategory
+            //{
+            //    CategoryId = 1,
+            //    ProductId = 2
+            //},
+            //new ProductCategory
+            //{
+            //    CategoryId = 3,
+            //    ProductId = 2,
+            //},
+            //new ProductCategory
+            //{
+            //    CategoryId = 4,
+            //    ProductId = 3,
+            //},
+            //new ProductCategory
+            //{
+            //    CategoryId = 2,
+            //    ProductId = 4,
+            //});
+
+            //modelBuilder.Entity<Event>().HasData(
+            //    new Event
+            //    {
+            //        Id = 1,
+            //        Title = "First Event",
+            //        Description = "Test event",
+            //        Price = 249.95M,
+            //        Created = DateTime.Now,
+            //        TimeofEvent = new DateTime(2023,5,2,23,23,00),
+            //    },
+            //    new Event
+            //    {
+            //        Id = 2,
+            //        Title = "Super Event",
+            //        Description = "Test 2 for testing making events",
+            //        Price = 546.95M,
+            //        Created = DateTime.Now,
+            //        TimeofEvent = new DateTime(2023, 9, 17, 13, 20, 00),
+            //    }); ;
+
+            //modelBuilder.Entity<NewsLike>().HasData(new NewsLike
+            //{
+            //    Id = 1,
+            //    user_Id = 1,
+            //    news_Id = 1,
+            //    DateTime = DateTime.Now,
+
+            //});
+
+            //modelBuilder.Entity<Music>().HasData(new Music
+            //{
+            //    Id = 1,
+            //    SongName = "Chipi chipi chapa chapa",
+            //    Album = "Chipi chipi",
+            //    SongFilePath = "assets/music/chibichibi.mp3",
+            //    SongPicturePath = "assets/img/chipichapa.gif"
+            //},
+            //new Music
+            //{
+            //    Id = 2,
+            //    SongName = "FlipFlop",
+            //    Album = "Bjørn",
+            //    SongFilePath = "assets/music/audio2.mp3",
+            //    SongPicturePath = ""
+            //},
+            //new Music
+            //{
+            //    Id = 3,
+            //    SongName = "Chatter",
+            //    Album = "Around the worlds",
+            //    SongFilePath = "assets/music/audio1.mp3",
+            //    SongPicturePath = ""
+            //});
+
+            //modelBuilder.Entity<ArtistSong>().HasData(new ArtistSong
+            //{
+            //    Id = 1,
+            //    ArtistId = 1,
+            //    MusicId = 1,
+            //},
+            //new ArtistSong
+            //{
+            //    Id = 2,
+            //    ArtistId = 1,
+            //    MusicId = 2,
+            //},
+            //new ArtistSong
+            //{
+            //    Id = 3,
+            //    ArtistId = 1,
+            //    MusicId = 3,
+            //});
+
+            //modelBuilder.Entity<ArtistLink>().HasData(new ArtistLink
+            //{
+            //    Id = 1,
+            //    ArtistId = 1,
+            //    LinkId = 1,
+            //});
+
+            //modelBuilder.Entity<Link>().HasData(new Link
+            //{
+            //    Id = 1,
+            //    Title = "Discord",
+            //    LinkAddress = "https://discord.gg/Jt4rwUZGGS"
+            //});
         }
     }
 }
