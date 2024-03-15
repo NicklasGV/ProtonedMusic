4d4b9d82fc3461eca7e3c8c56c8cc6e6cf6bc47c
Joey Oliver Høffding Knutsson <joey0177@elev.tec.dk>
2024-03-15 09:50:15 +0100

    Frontpage text not required and changed some namings with Music Panel

--- ProtonedMusic-API/ProtonedMusicAPI/DTO/FrontPageDTO/FrontPagePostRequest.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/DTO/FrontPageDTO/FrontPagePostRequest.cs
@@ -2,11 +2,9 @@
 {
     public class FrontpagePostRequest
     {
-        [Required]
         [StringLength(600, ErrorMessage = "Text cannot be longer than 500 characters")]
-        public string Text { get; set; }
-        [Required]
-        public Banner Banner { get; set; }
+        public string? Text { get; set; }
+        public Banner? Banner { get; set; }
         public IFormFile? PictureFile { get; set; }
         public string? FrontpagePicturePath { get; set; }
     }
--- ProtonedMusic-API/ProtonedMusicAPI/Database/Entities/FrontpagePost.cs
+++ ProtonedMusic-API/ProtonedMusicAPI/Database/Entities/FrontpagePost.cs
@@ -6,9 +6,9 @@
         public int Id { get; set; }
 
         [Column(TypeName = "nvarchar(500)")]
-        public string Text { get; set; }
+        public string? Text { get; set; }
 
-        public Banner Banner { get; set; }
+        public Banner? Banner { get; set; }
 
         public string? FrontpagePicturePath { get; set; }
 
